c368df6c037166820465ca39436d40f6a3007bfb
daisuke-shimura <daisuke.s0707@icloud.com>
2025-10-07 12:49:40 +0900

    Adminログイン

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  scope module: :public do
+    root to: "homes#top"
+  end
+
+  devise_for :admin ,controllers: {
+    sessions: "admin/sessions",
+    registrations: "admin/registrations"
+    
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
