0d5233cb7e73371527c7039ac0ea39e30e611e76
daisuke-shimura <daisuke.s0707@icloud.com>
2025-10-09 15:46:33 +0900

    Jobの提出はできる

--- app/controllers/public/jobs_controller.rb
+++ app/controllers/public/jobs_controller.rb
@@ -1,4 +1,35 @@
 class Public::JobsController < ApplicationController
   def index
+    @week = Week.find(params[:week_id])
+    #@users = User.all
+    @users = User.includes(:jobs).where(jobs: { week_id: @week.id })
+    @user = current_user
+    @job = Job.new
+  end
+
+  def create
+    job = Job.new(job_params)
+    week = Week.find(params[:week_id])
+    job.week_id = week.id
+    job.user_id = current_user.id
+    job.save
+    redirect_to request.referer
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+  def job_params
+    params.require(:job).permit(:time1, :time2, :time3, :time4, :time5, :time6, :time7, :comment)
   end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,8 @@
 class Job < ApplicationRecord
   belongs_to :user
   belongs_to :week
+
+  def job_for(week)
+    jobs.find { |job| job.week_id == week.id }
+  end
 end
