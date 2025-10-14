1d40659a5f95cefbac0443bb8c33b68174d9f153
daisuke-shimura <daisuke.s0707@icloud.com>
2025-10-14 11:18:56 +0900

    一旦ボタンを切り分ける

--- app/models/job.rb
+++ app/models/job.rb
@@ -6,7 +6,31 @@ class Job < ApplicationRecord
   validates :time1, :time2, :time3, :time4, :time5, :time6, :time7, :comment,
     presence: true, if: -> { [time1,time2,time3,time4,time5,time6,time7,comment].all?(&:blank?) }
   
-  # after_create_commit  -> { broadcast_append_to "jobs_week_#{week_id}" }
-  # after_update_commit  -> { broadcast_replace_to "jobs_week_#{week_id}" }
-  # after_destroy_commit -> { broadcast_remove_to "jobs_week_#{week_id}" }
+  
+  # after_create_commit  :broadcast_jobs_update
+  # after_update_commit  :broadcast_jobs_update
+  # after_destroy_commit :broadcast_jobs_destroy
+
+  # private
+  # def broadcast_jobs_update
+  #   users = User.all
+  #   jobs  = Job.where(week_id: week_id).group_by(&:user_id)
+  #   week.broadcast_replace_to(
+  #     "weeks",
+  #     target: "week_content_#{week_id}",
+  #     partial: "public/jobs/table",
+  #     locals: { week: week, users: users, jobs: jobs, login_user: user }
+  #   )
+  # end
+
+  # def broadcast_jobs_destroy
+  #   users = User.all
+  #   jobs  = Job.where(week_id: week_id).group_by(&:user_id)
+  #   week.broadcast_replace_to(
+  #     "weeks",
+  #     target: "week_content_#{week_id}",
+  #     partial: "public/jobs/table",
+  #     locals: { week: week, users: users, jobs: jobs, login_user: user }
+  #   )
+  # end
 end
