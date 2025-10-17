50de6f1093a1b8edc047f35283055afad9b71d97
daisuke-shimura <daisuke.s0707@icloud.com>
2025-10-18 00:58:25 +0900

    印刷ダイヤログ作成

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -5,4 +5,11 @@ class Admin::JobsController < ApplicationController
     @jobs = Job.where(week_id: @week.id).group_by(&:user_id)
     @setting = Setting.instance
   end
+
+  def print
+    @week = Week.find(params[:week_id])
+    @users = User.all
+    @jobs = Job.where(week_id: @week.id).group_by(&:user_id)
+    render layout: "print"
+  end
 end
